adc2761ae6dd1f7d320083ad6255822df3dcce8b
yx <prettypuppycavalier@gmail.com>
2022-11-16 21:18:11 +0800

    todo

---
+++ todo.ts
@@ -0,0 +1,14 @@
+/**
+ * @name: todo
+ * @author: yuxi
+ * @date: 2022/11/16 21:15
+ * @description：todo
+ * @update: 2022/11/16 21:15
+ */
+
+/**
+ * todo
+ * 作为npm库
+ * 倒计时，flex布局，copy功能，二维码解析/生成。时间格式化(思考支持任何格式)，file和bold互转
+ *
+ * */
